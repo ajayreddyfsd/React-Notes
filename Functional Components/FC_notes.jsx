6b780ca9f3b525c34d7ad135c07a640b2b89a83c
Ajay Reddy <ajayreddy.fsd@gmail.com>
2025-07-08 11:05:21 -0400

    component lifecycle methods complete

--- Functional Components/FC_notes.jsx
+++
@@ -1,9 +0,0 @@
-// Writing a simple functional component
-
-// Using that component as a tag in JSX
-
-// Passing props as inputs via attributes
-
-// handling props inside the component
-
-// also the components folder shd be inside the src folder
