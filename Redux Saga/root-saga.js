9bd170d266da4e5bce29b44cf69767cc0ce6d341
Ajay Reddy <ajayreddy.fsd@gmail.com>
2025-08-07 15:28:09 -0400

    redux saga complete

--- Redux Saga/root-saga.js
+++ Redux Saga/root-saga.js
@@ -2,7 +2,7 @@
 import { all, call } from "redux-saga/effects";
 
 //this is one of the saga files that we wrote in store/categories
-import { categoriesSaga } from "./categories/category.saga";
+import { categoriesSaga } from "./categories (using redux saga)/category.saga";
 
 //this is the root saga function
 //calls all the sepcified sagas and runs them parallely and yields results,
