e9d0c910ddb5e264a2062afc39ab1fb8f399983a
Ajay Reddy <ajayreddy.fsd@gmail.com>
2025-08-08 04:53:39 -0400

    modified user-folder based on redux saga

--- Redux Saga/user (using redux saga)/user.reducer.js
+++ Redux Saga/user (using redux saga)/user.reducer.js
@@ -1,4 +1,4 @@
-import { USER_ACTION_TYPES } from './user.types';
+import { USER_ACTION_TYPES } from "./user.types";
 
 const INITIAL_STATE = {
   currentUser: null,
@@ -10,14 +10,47 @@ export const userReducer = (state = INITIAL_STATE, action) => {
   const { type, payload } = action;
 
   switch (type) {
+    // When an async user action starts, set loading true & clear errors
+    case USER_ACTION_TYPES.GOOGLE_SIGN_IN_START:
+    case USER_ACTION_TYPES.EMAIL_SIGN_IN_START:
+    case USER_ACTION_TYPES.SIGN_UP_START:
+    case USER_ACTION_TYPES.SIGN_OUT_START:
+    case USER_ACTION_TYPES.CHECK_USER_SESSION:
+      return {
+        ...state,
+        isLoading: true,
+        error: null,
+      };
+
+    // On success, set loading false and update currentUser (or clear on sign out)
     case USER_ACTION_TYPES.SIGN_IN_SUCCESS:
-      return { ...state, currentUser: payload };
+    case USER_ACTION_TYPES.SIGN_UP_SUCCESS:
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return {
+        ...state,
+        isLoading: false,
+        currentUser: payload.user ?? payload, // to support both shaped payloads
+        error: null,
+      };
+
     case USER_ACTION_TYPES.SIGN_OUT_SUCCESS:
-      return { ...state, currentUser: null };
-    case USER_ACTION_TYPES.SIGN_OUT_FAILED:
+      return {
+        ...state,
+        isLoading: false,
+        currentUser: null,
+        error: null,
+      };
+
+    // On failure, set loading false and store error
     case USER_ACTION_TYPES.SIGN_IN_FAILED:
     case USER_ACTION_TYPES.SIGN_UP_FAILED:
-      return { ...state, error: payload };
+    case USER_ACTION_TYPES.SIGN_OUT_FAILED:
+      return {
+        ...state,
+        isLoading: false,
+        error: payload,
+      };
+
     default:
       return state;
   }
--- Redux Saga/user (using redux saga)/user.saga.js
+++ Redux Saga/user (using redux saga)/user.saga.js
@@ -20,6 +20,17 @@ import {
   signOutUser,
 } from "../../utils/firebase/firebase.utils";
 
+//? helper sagas start
+//? helper sagas start
+//? helper sagas start
+//? helper sagas start
+//? helper sagas start
+
+//! this helper saga, takes in userAuth object as input
+//! checks if it exist in the firestoreDB and fetches the userSnapshot (or)
+//! creates new user doc in the firestoreDB and fetches the userSnapshot
+//! Then dispatches either signInSuccess (with user data) or signInFailed if an error happens.
+//! The dispatched action updates the global-Redux-user-state accordingly.
 export function* getSnapshotFromUserAuth(userAuth, additionalDetails) {
   try {
     const userSnapshot = yield call(
@@ -33,28 +44,46 @@ export function* getSnapshotFromUserAuth(userAuth, additionalDetails) {
   }
 }
 
+//! This helper saga calls the appropriate Firebase util function to sign in the user (Google popup or email/password).
+//! and Firebase returns a userAuth object upon successful sign-in.
+//! This userAuth object is passed to getSnapshotFromUserAuth, which:
+//! Creates user doc in firestore if it did not exist or fetches the user document from Firestore it it already exists,
+//! Then dispatches either signInSuccess (with user data) or signInFailed if an error happens.
+//! The dispatched action updates the global-Redux-user-state accordingly.
 export function* signInWithGoogle() {
   try {
-    const { user } = yield call(signInWithGooglePopup);
-    yield call(getSnapshotFromUserAuth, user);
+    const { user: userAuth } = yield call(signInWithGooglePopup);
+    yield call(getSnapshotFromUserAuth, userAuth);
   } catch (error) {
     yield put(signInFailed(error));
   }
 }
 
+//! This helper saga calls the appropriate Firebase util function to sign in the user (Google popup or email/password).
+//! and Firebase returns a userAuth object upon successful sign-in.
+//! This userAuth object is passed to getSnapshotFromUserAuth, which:
+//! Creates user doc in firestore if it did not exist or fetches the user document from Firestore it it already exists,
+//! Then dispatches either signInSuccess (with user data) or signInFailed if an error happens.
+//! The dispatched action updates the global-Redux-user-state accordingly.
 export function* signInWithEmail({ payload: { email, password } }) {
   try {
-    const { user } = yield call(
+    const { user: userAuth } = yield call(
       signInAuthUserWithEmailAndPassword,
       email,
       password
     );
-    yield call(getSnapshotFromUserAuth, user);
+    yield call(getSnapshotFromUserAuth, userAuth);
   } catch (error) {
     yield put(signInFailed(error));
   }
 }
 
+//! this helper saga checks if we have any current user using getCurrentUser()
+//! and gets the usersnapshot if we have one, or else returns quietly
+//! if error, then signInFailed
+//? why cant we check if we have current user from redux-global-state
+//? we can do that too, as we have redux-persist installed, but we are doing this for the 'just in case' case
+//? in case we lost the session data, or we have an expired-session-data or the session has been revoked by backend
 export function* isUserAuthenticated() {
   try {
     const userAuth = yield call(getCurrentUser);
@@ -65,6 +94,11 @@ export function* isUserAuthenticated() {
   }
 }
 
+//! simple signup by calling apt firebase util func
+//! and dispatch actions accordingly
+//! but what about storing this new user into firestore db
+//! we will do this in next helper-saga-func "signInAfterSignUp"
+//? why? in sep step, coz we wanna store only if signup is success only
 export function* signUp({ payload: { email, password, displayName } }) {
   try {
     const { user } = yield call(
@@ -78,6 +112,12 @@ export function* signUp({ payload: { email, password, displayName } }) {
   }
 }
 
+//! this is purely to store the above new user into firestore db when we saga-listener hears the sign-up-success action-type
+export function* signInAfterSignUp({ payload: { user, additionalDetails } }) {
+  yield call(getSnapshotFromUserAuth, user, additionalDetails);
+}
+
+//! simple signout helper saga dispatching actions accordingly
 export function* signOut() {
   try {
     yield call(signOutUser);
@@ -87,16 +127,22 @@ export function* signOut() {
   }
 }
 
-export function* signInAfterSignUp({ payload: { user, additionalDetails } }) {
-  yield call(getSnapshotFromUserAuth, user, additionalDetails);
-}
+//? helper sagas end
+//? helper sagas end
+//? helper sagas end
+//? helper sagas end
+//? helper sagas end
 
+//? saga listeners start
+//? saga listeners start
+//? saga listeners start
+//? saga listeners start
+//? saga listeners start
+
+//! saga listeners listening to specific actions,
+//! which when listened, runs the specified saga-helper-functions
 export function* onGoogleSignInStart() {
   yield takeLatest(USER_ACTION_TYPES.GOOGLE_SIGN_IN_START, signInWithGoogle);
-}
-
-export function* onCheckUserSession() {
-  yield takeLatest(USER_ACTION_TYPES.CHECK_USER_SESSION, isUserAuthenticated);
 }
 
 export function* onEmailSignInStart() {
@@ -115,6 +161,17 @@ export function* onSignOutStart() {
   yield takeLatest(USER_ACTION_TYPES.SIGN_OUT_START, signOut);
 }
 
+export function* onCheckUserSession() {
+  yield takeLatest(USER_ACTION_TYPES.CHECK_USER_SESSION, isUserAuthenticated);
+}
+
+//? saga listeners end
+//? saga listeners end
+//? saga listeners end
+//? saga listeners end
+//? saga listeners end
+
+//! initiating all the above saga based generator function listeners
 export function* userSagas() {
   yield all([
     call(onCheckUserSession),
