ead1b2c2bdb6f38dc83a849da97a8e4df26df827
Ajay Reddy <ajayreddy.fsd@gmail.com>
2025-08-09 05:44:09 -0400

    done

--- Redux Saga/categories (using redux saga)/thunk_flow.jsx
+++
@@ -1,6 +0,0 @@
-// [Shop component]
-//   dispatch(fetchCategoriesStartAsync())  -->  // calls thunk function and not dispatching any action
-//     thunk runs async code inside itself
-//       dispatch(fetchCategoriesStart())      --> sends "start" action to reducer
-//       fetch data from API
-//       dispatch(fetchCategoriesSuccess())    --> sends "success" action to reducer
